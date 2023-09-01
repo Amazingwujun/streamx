a5f317ef9025f730eb9e7775e191b8731841a3b1
Amazingwujun <85998282@qq.com>
2023-09-01 17:30:29 +0800

    perf: 优化 RtmpMessage 对象 payload 处理方式

--- broker/src/main/java/com/jun/streamx/broker/entity/RtmpMessage.java
+++ broker/src/main/java/com/jun/streamx/broker/entity/RtmpMessage.java
@@ -1,7 +1,12 @@
 package com.jun.streamx.broker.entity;
 
+import com.jun.streamx.broker.constants.Amf0Marker;
 import com.jun.streamx.broker.constants.RtmpMessageType;
+import com.jun.streamx.commons.entity.Tuple2;
 import io.netty.buffer.ByteBuf;
+import io.netty.util.ReferenceCounted;
+
+import java.util.List;
 
 /**
  * rtmp 消息.
@@ -9,7 +14,7 @@ import io.netty.buffer.ByteBuf;
  * @author Jun
  * @since 1.0.0
  */
-public class RtmpMessage {
+public class RtmpMessage implements ReferenceCounted {
 
     //@formatter:off
 
@@ -55,18 +60,102 @@ public class RtmpMessage {
         return payload;
     }
 
-    public void release() {
-        this.payload.release();
+    /**
+     * @see ByteBuf#readBytes(byte[])
+     */
+    public void readBytes(byte[] dst) {
+        payload.readBytes(dst);
+    }
+
+    /**
+     * 读取全部数据
+     *
+     * @return payload 中的全部字节数组
+     */
+    public byte[] readBytes() {
+        return readBytes(payload.readableBytes());
+    }
+
+    /**
+     * 读取指定长度 buf
+     *
+     * @param len 需要读取字节长度
+     */
+    public byte[] readBytes(int len) {
+        var buf = new byte[len];
+        payload.readBytes(buf);
+        return buf;
+    }
+
+
+    @Override
+    public int refCnt() {
+        return this.payload.refCnt();
+    }
+
+    @Override
+    public ReferenceCounted retain() {
+        return this.payload.retain();
+    }
+
+    @Override
+    public ReferenceCounted retain(int increment) {
+        return this.payload.retain(increment);
+    }
+
+    @Override
+    public ReferenceCounted touch() {
+        return this.payload.touch();
+    }
+
+    @Override
+    public ReferenceCounted touch(Object hint) {
+        return this.payload.touch(hint);
+    }
+
+    @Override
+    public boolean release() {
+        return this.payload.release();
+    }
+
+    @Override
+    public boolean release(int decrement) {
+        return this.payload.release(decrement);
+    }
+
+    /**
+     * 将当前的 payload 解析为 AMF0 格式的数据
+     *
+     * @return AMF0 格式数据
+     */
+    public List<Tuple2<Amf0Marker, Object>> payloadToAmf0() {
+        return Amf0Format.parse(payload);
     }
 
     @Override
     public String toString() {
-        return "RtmpMessage{" +
-                "messageType=" + messageType +
-                ", payloadLength=" + payloadLength +
-                ", timestamp=" + timestamp +
-                ", streamId=" + streamId +
-                ", payload=" + payload +
-                '}';
+        switch (messageType) {
+            case AMF0_DATA, AMF0_COMMAND -> {
+                this.payload.markReaderIndex();
+                var amf0 = payloadToAmf0();
+                this.payload.resetReaderIndex();
+                return "RtmpMessage{" +
+                        "messageType=" + messageType +
+                        ", payloadLength=" + payloadLength +
+                        ", timestamp=" + timestamp +
+                        ", streamId=" + streamId +
+                        ", payload=" + amf0 +
+                        '}';
+            }
+            default -> {
+                return "RtmpMessage{" +
+                        "messageType=" + messageType +
+                        ", payloadLength=" + payloadLength +
+                        ", timestamp=" + timestamp +
+                        ", streamId=" + streamId +
+                        ", payload=" + payload +
+                        '}';
+            }
+        }
     }
 }
