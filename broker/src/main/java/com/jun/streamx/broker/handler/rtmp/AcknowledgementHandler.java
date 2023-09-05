42ad84b8b0e0a564de6ca1bf144c5d95cd2b221e
Amazingwujun <85998282@qq.com>
2023-09-05 21:16:02 +0800

    feat: AcknowledgementHandler add

---
+++ broker/src/main/java/com/jun/streamx/broker/handler/rtmp/AcknowledgementHandler.java
@@ -0,0 +1,22 @@
+package com.jun.streamx.broker.handler.rtmp;
+
+import com.jun.streamx.broker.constants.RtmpMessageType;
+import com.jun.streamx.broker.entity.RtmpMessage;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * {@link RtmpMessageType#ACKNOWLEDGEMENT}
+ *
+ * @author Jun
+ * @since 1.0.0
+ */
+@Slf4j
+@Handler(type = RtmpMessageType.ACKNOWLEDGEMENT)
+public class AcknowledgementHandler implements MessageHandler {
+
+    @Override
+    public void process(ChannelHandlerContext ctx, RtmpMessage msg) {
+        log.info("收到 Acknowledgement 报文: {}", msg);
+    }
+}
